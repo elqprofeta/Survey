13cbf6b1d0af23dbdf68df3ab43e57f6d92b461a
elqprofeta <eequinteroz@yahoo.com>
2025-03-05 21:41:26 -0500

    supremacy--

--- survei.py
+++ survei.py
@@ -100,13 +100,16 @@ firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
 print (firebase_credentials)
 print(f" credenciales de Firebase: {firebase_credentials}")
 
+print(repr(firebase_credentials))  # Muestra la forma real de la cadena ***
 
-# if firebase_credentials:
+
+ if firebase_credentials:
     cred_dict = json.loads(firebase_credentials)  # Convertir la cadena en un diccionario Python
     cred = credentials.Certificate(cred_dict)    # Usar credenciales desde diccionario
     firebase_admin.initialize_app(cred)          # # Inicializar Firebase con las credenciales
-# else:
-#    raise ValueError(" No se encontraron credenciales de Firebase en las variables de entorno ")
+ else:
+    raise ValueError(" No se encontraron credenciales de Firebase en las variables de entorno ")
+    print(firebase_cred_json[:500])  # Muestra solo una parte para evitar exponer credenciales
 #    del os.environ["FIREBASE_CREDENTIALS"]  # Eliminamos la variable
 #   ⚠️
 
